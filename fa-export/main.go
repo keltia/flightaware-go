de650c9794f13980ac98750c39fe5f34475398fa
Ollivier Robert <roberto@keltia.net>
2015-04-28 15:00:57 +0200

    Use the new Config module.

--- fa-export/main.go
+++ fa-export/main.go
@@ -6,9 +6,16 @@ package main
 
 import (
 	"../flightaware"
+	"../config"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/codegangsta/cli"
+)
+
+var (
+	RcFile = filepath.Join(os.Getenv("HOME"), ".flightaware", "config.yml")
 )
 
 func main() {
@@ -17,6 +24,14 @@ func main() {
 	app.Author = "Ollivier Robert"
 	app.Version = "0.0.1"
 	app.Usage = "fa-export"
+
 	fmt.Println("Hello world\n", app.Name)
 	flightaware.HelloWorld()
+
+	c, err := config.LoadConfig(RcFile)
+	if err != nil {
+		fmt.Println("Error loading")
+	}
+	fmt.Println(c.Dests)
+	fmt.Println(c.Default, c.Dests[c.Default])
 }
