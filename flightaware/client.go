02016b6d252b685a3ac05ea78991b55df59a227c
Ollivier Robert <roberto@keltia.net>
2015-04-28 17:00:14 +0200

    Put more meat on flightaware's bone.

--- flightaware/client.go
+++ flightaware/client.go
@@ -5,15 +5,37 @@
 package flightaware
 
 import (
-	//"crypto/tls"
-	//"net/http"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+
+	"../config"
 )
+
+type Client struct {
+	Host	config.Config
+	Bytes	int64
+	Pkts	int32
+}
 
 func HelloWorld() {
 	fmt.Println("Hello package")
 }
 
-func NewClient() {
+func NewClient(rc config.Config) (Client, error) {
+	cl := Client{rc, 0, 0}
 
+	str := fmt.Sprintf("%v:%v\n", rc.Site, rc.Port)
+	fmt.Printf("Connecting to %v\n", str)
+
+	roots := x509.NewCertPool()
+	conn, err := tls.Dial("tcp", str, &tls.Config{
+			RootCAs: roots,
+		})
+	if err != nil {
+		panic("failed to connect: " + err.Error())
+	}
+
+	conn.Close()
+	return cl, err
 }
