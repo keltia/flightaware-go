2a74614d3ddab8dd9274fa4c3b2f435d19f2fad8
Ollivier Robert <roberto@keltia.net>
2015-06-17 11:12:47 +0200

    Add cli.go from surv-client-go.

---
+++ fa-export/cli.go
@@ -0,0 +1,40 @@
+// cli.go
+//
+// Everything related to command-line flag handling
+//
+// Copyright 2015 © by Ollivier Robert for the EEC
+//
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	// cli
+	fVerbose	bool
+	fOutput		string
+)
+
+// my usage string
+const (
+	cliUsage	= `
+Usage: %s [-o FILE] [-v]
+`
+)
+
+// Redefine Usage
+var Usage = func() {
+        fmt.Fprintf(os.Stderr, cliUsage, os.Args[0])
+        flag.PrintDefaults()
+}
+
+// called by flag.Parse()
+func init() {
+	// cli
+	flag.BoolVar(&fVerbose, "v", false, "Set verbose flag.")
+	flag.StringVar(&fOutput, "o", "", "Specify output FILE.")
+}
