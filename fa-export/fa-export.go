a0b0ac9645c134c1aa796efeb4867c0543a1feb4
Ollivier Robert <roberto@keltia.net>
2015-09-24 14:04:39 +0200

    Implement saving for previous/existing out files with newly added -O.

--- fa-export/fa-export.go
+++ fa-export/fa-export.go
@@ -173,8 +173,27 @@ func main() {
 			log.Printf("Output file is %s\n", fOutput)
 		}
 
+		// Check if the file already exist
+		if fi, err := os.Stat(fOutput); err != nil {
+			if fVerbose {
+				log.Printf("Warning: %s (%v) already exists!", fOutput, fi.ModTime())
+				if fOverwrite {
+					log.Println("… overwriting it.")
+				}
+			}
+			// Default for fOverwrite is false so we save the file
+			if !fOverwrite {
+				newFile := fmt.Sprintf("%s.old", fOutput)
+				os.Rename(fOutput, newFile)
+				if fVerbose {
+					log.Printf("Info: %s renamed into %s\n", fOutput, newFile)
+				}
+			}
+		}
+
+		//Default for Create() is to overwrite if already exist
 		if fOutputFH, err = os.Create(fOutput); err != nil {
-			log.Printf("Error creating %s\n", fOutput)
+			log.Printf("Error: can not create/overwrite %s\n", fOutput)
 			panic(err)
 		}
 
