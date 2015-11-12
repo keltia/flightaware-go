48189c1b28700bc291eb3133dac8b488128885f2
Ollivier Robert <roberto@keltia.net>
2015-11-12 16:23:05 +0100

    Complete record decoding (v5 ATM, hopefully compatible).

--- flightaware/decode.go
+++ flightaware/decode.go
@@ -22,23 +22,65 @@ func getType (record []byte) (string, error) {
 	return generic.Type, nil
 }
 
-func decodeRecord(record []byte) (interface{}) {
+func decodeRecord(record []byte) (interface{}, error) {
 	var generic interface{}
 
 	// Check input record
 	if err := json.Unmarshal(record, &generic); err != nil {
-		return errors.New(fmt.Sprintf("Unable to decode %v: %v\n", record, err))
+		return nil, errors.New(fmt.Sprintf("Unable to decode %v: %v\n", record, err))
 	}
 
 	payload := generic.(map[string]interface{})
 	switch payload["type"] {
 	case "position":
-
-
+        return decodePosition(payload)
+	case "flightplan":
+		return decodeFlightplan(payload)
+	case "departure":
+		return decodeDeparture(payload)
+	case "arrival":
+		return decodeArrival(payload)
+	case "cancellation":
+		return decodeCancellation(payload)
+	default:
+		return nil, errors.New(fmt.Sprintf("Unknown record type %s", payload["type"]))
 	}
-	return nil
+	return nil, nil
 }
 
+func decodeFlightplan(record map[string]interface{}) (FAflightplan, error) {
+
+	generic := FAflightplan{}
+	for k, v := range record {
+		switch k {
+		// mandatory fields
+		case "type": generic.Type = v.(string)
+		case "ident": generic.Ident = v.(string)
+		case "status": generic.Status = v.(string)
+		case "orig": generic.Orig = v.(string)
+		case "dest": generic.Dest = v.(string)
+		case "edt": generic.Edt = v.(string)
+		case "eta": generic.Eta = v.(string)
+		case "ete": generic.Ete = v.(string)
+		case "id": generic.Id = v.(string)
+		// common
+		case "aircrafttype": generic.AircraftType = v.(string)
+		case "suffix": generic.Suffix = v.(string)
+		case "reg": generic.Reg = v.(string)
+		case "speed": generic.Speed = v.(string)
+		case "alt": generic.Alt = v.(string)
+		case "facility_hash": generic.FacilityHash = v.(string)
+		case "facility_name": generic.FacilityName = v.(string)
+		// optional
+		case "prefix": generic.Prefix = v.(string)
+		case "waypoints": generic.Waypoints = v.([]FApoint)
+		case "FDwaypoints": generic.FDWaypoints = v.([]FApoint)
+		case "route": generic.Route = v.(string)
+		case "atcident": generic.Atcident = v.(string)
+		}
+	}
+	return generic, nil
+}
 func decodePosition(record map[string]interface{}) (FAposition, error) {
 
 	generic := FAposition{}
@@ -78,42 +120,72 @@ func decodePosition(record map[string]interface{}) (FAposition, error) {
 	return generic, nil
 }
 
-func decodeDeparture(record []byte) (FAdeparture, error) {
+func decodeDeparture(record map[string]interface{}) (FAdeparture, error) {
 
 	generic := FAdeparture{}
-	// Check input record
-	if err := json.Unmarshal(record, &generic); err != nil {
-		return generic, errors.New(fmt.Sprintf("Unable to decode %v: %v\n", record, err))
+	for k, v := range record {
+		switch k {
+		// mandatory fields
+		case "type": generic.Type = v.(string)
+		case "ident": generic.Ident = v.(string)
+		case "orig": generic.Orig = v.(string)
+		case "dest": generic.Dest = v.(string)
+		case "adt": generic.Eta = v.(string)
+		case "eta": generic.Eta = v.(string)
+		case "id": generic.Id = v.(string)
+		// common fields
+		case "aircrafttype": generic.AircraftType = v.(string)
+		case "facility_hash": generic.FacilityHash = v.(string)
+		case "facility_name": generic.FacilityName = v.(string)
+		// optional
+		case "synthetic": generic.Synthetic = v.(string)
+		case "atcident": generic.Atcident = v.(string)
+		}
 	}
 	return generic, nil
 }
 
-func decodeArrival(record []byte) (FAarrival, error) {
+func decodeArrival(record map[string]interface{}) (FAarrival, error) {
 
 	generic := FAarrival{}
-	// Check input record
-	if err := json.Unmarshal(record, &generic); err != nil {
-		return generic, errors.New(fmt.Sprintf("Unable to decode %v: %v\n", record, err))
+	for k, v := range record {
+		switch k {
+		// mandatory fields
+		case "type": generic.Type = v.(string)
+		case "ident": generic.Ident = v.(string)
+		case "orig": generic.Orig = v.(string)
+		case "dest": generic.Dest = v.(string)
+		case "aat": generic.Aat = v.(string)
+		case "timeType": generic.timeType = v.(string)
+		case "id": generic.Id = v.(string)
+		// common
+		case "facility_hash": generic.FacilityHash = v.(string)
+		case "facility_name": generic.FacilityName = v.(string)
+		// optional
+		case "synthetic": generic.Synthetic = v.(string)
+		case "atcident": generic.Atcident = v.(string)
+		}
 	}
 	return generic, nil
 }
 
-func decodeCancellation(record []byte) (FAcancellation, error) {
+func decodeCancellation(record map[string]interface{}) (FAcancellation, error) {
 
 	generic := FAcancellation{}
-	// Check input record
-	if err := json.Unmarshal(record, &generic); err != nil {
-		return generic, errors.New(fmt.Sprintf("Unable to decode %v: %v\n", record, err))
-	}
-	return generic, nil
-}
-
-func decodeFlightplan(record []byte) (FAflightplan, error) {
-
-	generic := FAflightplan{}
-	// Check input record
-	if err := json.Unmarshal(record, &generic); err != nil {
-		return generic, errors.New(fmt.Sprintf("Unable to decode %v: %v\n", record, err))
+	for k, v := range record {
+		switch k {
+		// mandatory fields
+		case "type": generic.Type = v.(string)
+		case "ident": generic.Ident = v.(string)
+		case "orig": generic.Orig = v.(string)
+		case "dest": generic.Dest = v.(string)
+		case "id": generic.Id = v.(string)
+		// common
+		case "facility_hash": generic.FacilityHash = v.(string)
+		case "facility_name": generic.FacilityName = v.(string)
+		// optional
+		case "atcident": generic.Atcident = v.(string)
+		}
 	}
 	return generic, nil
 }
