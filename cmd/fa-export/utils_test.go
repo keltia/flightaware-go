e49b44707235c4bb669fb767b6a71e1394f9b758
Ollivier Robert <roberto@keltia.net>
2018-08-22 11:12:55 +0200

    Fix test.

--- cmd/fa-export/utils_test.go
+++ cmd/fa-export/utils_test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"testing"
 	"errors"
+	"testing"
+	"time"
 )
-
 
 func TestAnalyzeFormat(t *testing.T) {
 	_, err := AnalyzeFormat("foo")
@@ -17,7 +17,7 @@ func TestAnalyzeFormat(t *testing.T) {
 		t.Errorf("Error: %s", err.Error())
 	}
 
-	day  := Rotation{RT_DAY}
+	day := Rotation{RT_DAY}
 	hour := Rotation{RT_HOUR}
 
 	ret, err := AnalyzeFormat("%d")
@@ -38,7 +38,7 @@ func TestParseDate(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error: bad parsing for 2015-06-09 00:00:00: %v", err.Error())
 	}
-	if tm != 1433800800 {
+	if tm != time.Unix(1433800800, 0) {
 		t.Errorf("Error: bad parsing for 2015-06-09 00:00:00: %v", errors.New("bad value"))
 	}
 }
